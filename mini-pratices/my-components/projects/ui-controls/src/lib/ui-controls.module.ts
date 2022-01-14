cd9ae518de35d385f24e494d3ce46dcf560871dd
Leonardo Leyva <leoshield9@gmail.com>
2022-01-13 20:32:18 -0700

    update my components

---
+++ mini-pratices/my-components/projects/ui-controls/src/lib/ui-controls.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { UiControlsComponent } from './ui-controls.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { CommonModule } from "@angular/common";
+import { DragDropModule } from "@angular/cdk/drag-drop";
+
+@NgModule({
+  declarations: [
+    UiControlsComponent,
+    CardListComponent
+  ],
+  imports: [
+    CommonModule,  // Necesaria para la directiva *ngFor
+    DragDropModule // Usado para arrastrar y soltar
+  ],
+  exports: [
+    UiControlsComponent,
+    CardListComponent
+  ]
+})
+export class UiControlsModule { }
---
+++ mini-pratices/my-components/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { UiControlsModule } from "ui-controls";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    UiControlsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
